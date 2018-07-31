e1d0927bb3cdca1e7b86f867851f253339949288
nevoband <nevoband@uic.edu>
2018-07-31 16:41:21 -0500

    Updated college filter to bring in employees whos college is pharmacy but job may not be

--- lib/edw_filter.py
+++ lib/edw_filter.py
@@ -22,14 +22,16 @@ class EdwFilter:
         if len(class_codes) > 0:
             for idx, classCode in enumerate(class_codes):
                 if "%" not in class_codes:
-                    class_code_conditionals.append("V_JOB_DETL_HIST_1.JOB_DETL_EMPEE_CLS_CD LIKE :class_code_" + str(idx))
+                    class_code_conditionals.append(
+                        "V_JOB_DETL_HIST_1.JOB_DETL_EMPEE_CLS_CD LIKE :class_code_" + str(idx))
                 else:
                     class_code_conditionals.append("V_JOB_DETL_HIST_1.JOB_DETL_EMPEE_CLS_CD = :class_code_" + str(idx))
                 class_code_filter_dict["class_code_" + str(idx)] = str(classCode)
             self.add_filter("( " + ' OR '.join(class_code_conditionals) + " )", class_code_filter_dict)
 
     def filter_college(self, college_code):
-        college_conditional = "V_JOB_DETL_HIST_1.JOB_DETL_COLL_CD = :coll_code"
+        college_conditional = "(V_JOB_DETL_HIST_1.JOB_DETL_COLL_CD = :coll_code OR " \
+                              "V_EMPEE_HIST_1.EMPEE_COLL_CD = :coll_code)"
         college_filter_dict = {"coll_code": str(college_code)}
         self.add_filter(college_conditional, college_filter_dict)
 
@@ -38,7 +40,8 @@ class EdwFilter:
         exclude_netid_dict = {}
         if len(netids) > 0:
             for idx, netid in enumerate(netids):
-                exclude_netid_conditionals.append("V_EMPEE_CAMPUS_EMAIL_ADDR.EMAIL_ADDR NOT LIKE :netid_exclude_" + str(idx))
+                exclude_netid_conditionals.append(
+                    "V_EMPEE_CAMPUS_EMAIL_ADDR.EMAIL_ADDR NOT LIKE :netid_exclude_" + str(idx))
                 exclude_netid_dict["netid_exclude_" + str(idx)] = str(netid) + '@%'
             self.add_filter(" AND ".join(exclude_netid_conditionals), exclude_netid_dict)
 
